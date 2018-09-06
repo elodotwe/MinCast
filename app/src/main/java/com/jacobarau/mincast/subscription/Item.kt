42d514e9889fa01855046d31f6c6e381bc6bf94b
Jacob Rau <jarau@mtu.edu>
2018-09-06 07:56:52 -0400

    Document the Item properties, make some not nullable

--- app/src/main/java/com/jacobarau/mincast/subscription/Item.kt
+++ app/src/main/java/com/jacobarau/mincast/subscription/Item.kt
@@ -9,14 +9,53 @@ import org.threeten.bp.Instant
         parentColumns = ["url"],
         childColumns = ["parentUrl"])])
 class Item {
+    /**
+     * Internal database ID for this item. Not tied to any value in the RSS.
+     */
     @PrimaryKey(autoGenerate = true)
-    var id: Int? = null
-    var parentUrl: String? = null
+    var id: Int = 0
+
+    /**
+     * The URL of the Subscription's URL, the URL to the RSS file we fetched.
+     */
+    var parentUrl: String = ""
+
+    /**
+     * Human-readable title of this item, if present in RSS.
+     *
+     * Either title or description will be present at a bare minimum.
+     */
     var title: String? = null
+
+    /**
+     * Human-readable description of this item, if present in RSS.
+     *
+     * Either title or description will be present at a bare minimum.
+     */
     var description: String? = null
+
+    /**
+     * Date-timestamp of when this item was published; null if not present in the RSS
+     */
     var publishDate: Instant? = null
+
+    /**
+     * If present in the RSS, the URL to the enclosure (typically the podcast audio goes here)
+     */
     var enclosureUrl: String? = null
+
+    /**
+     * MIME type of the enclosure.
+     *
+     * If enclosure URL is not null, this will be not null as well.
+     */
     var enclosureMimeType: String? = null
+
+    /**
+     * Length in bytes of the enclosure.
+     *
+     * If enclosure URL is not null, this will be not null as well.
+     */
     var enclosureLengthBytes: Int? = null
 
     override fun toString(): String {

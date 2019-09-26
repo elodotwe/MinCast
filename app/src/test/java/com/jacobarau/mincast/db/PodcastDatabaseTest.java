0854a7a8fafa2acb6938fe3773fdbe73b9a682df
Jacob Rau <jarau@mtu.edu>
2019-09-25 22:44:35 -0400

    Fix broken database tests

--- app/src/test/java/com/jacobarau/mincast/db/PodcastDatabaseTest.java
+++ app/src/test/java/com/jacobarau/mincast/db/PodcastDatabaseTest.java
@@ -12,11 +12,7 @@ import org.robolectric.RuntimeEnvironment;
 import org.threeten.bp.Instant;
 import org.threeten.bp.temporal.ChronoUnit;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @RunWith(RobolectricTestRunner.class)
 public class PodcastDatabaseTest {
@@ -25,23 +21,6 @@ public class PodcastDatabaseTest {
         PodcastDatabaseHelper helper = new PodcastDatabaseHelper(RuntimeEnvironment.application.getApplicationContext());
         SQLiteDatabase database = helper.getWritableDatabase();
         PodcastDatabase podcastDatabase = new PodcastDatabase(database);
-
-        final ConcurrentLinkedQueue<List<Subscription>> callbacks = new ConcurrentLinkedQueue<>();
-
-        podcastDatabase.addSubscriptionsObserver(new Observer() {
-            @Override
-            public void update(Observable o, Object arg) {
-                @SuppressWarnings("unchecked") List<Subscription> subscriptions = (List<Subscription>) arg;
-                System.out.println("Subs updated");
-                System.out.println(Arrays.toString(subscriptions.toArray()));
-                callbacks.add(subscriptions);
-            }
-        });
-
-        Assert.assertEquals(1, callbacks.size());
-        //noinspection ConstantConditions
-        Assert.assertEquals(0, callbacks.poll().size());
-
 
         Subscription subscription = new Subscription();
         subscription.setLastUpdated(Instant.now().truncatedTo(ChronoUnit.SECONDS));
@@ -52,9 +31,7 @@ public class PodcastDatabaseTest {
         subscription.setUrl("key url");
         podcastDatabase.addSubscription(subscription);
 
-        Assert.assertEquals(1, callbacks.size());
-        List<Subscription> subscriptions = callbacks.poll();
-        //noinspection ConstantConditions
+        List<Subscription> subscriptions = podcastDatabase.getSubscriptions();
         Assert.assertEquals(1, subscriptions.size());
         Assert.assertEquals(subscription, subscriptions.get(0));
     }
